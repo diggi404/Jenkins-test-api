52f225c52ce0518888beda9ec7b23ef5453771c6
diggi404 <dbackson1@gmail.com>
2024-02-27 14:35:05 -0500

    dockerfile included,new api response

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello Jenkins."));
+app.get("/", (req, res) =>
+  res.send("Hello Jenkins with Docker and deployed to Heroku.")
+);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
