aa2dd7d843681133c9f2b98f37e96fcf662345a3
diggi404 <dbackson1@gmail.com>
2024-02-28 11:16:59 -0500

    updated api response

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Welcome to the main endpoint."));
+app.get("/", (req, res) => res.send("Welcome to the main endpoint"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
